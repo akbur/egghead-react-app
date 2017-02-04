df7ecacb23ca078c8c5fe2537821748006781a13
Kris Burke <kris.burke611@gmail.com>
2017-02-03 23:26:49 -0500

    Up to lesson 13

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoForm, TodoList } from './components/todo'
+import { addTodo, generateId, findById, toggleTodo, updateTodo } from './lib/todoHelpers';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, name: 'Learn JSX', isComplete: true},
+      {id: 2, name: 'Build an Awesome App', isComplete: false},
+      {id: 3, name: 'Ship it!', isComplete: false}
+    ],
+    currentTodo: ''
+  };
+
+  handleInputChange = (event) => {
+    this.setState({
+      currentTodo: event.target.value
+    });
+  }
+
+  handleToggle = (id) => {
+    const todo = findById(id, this.state.todos);
+    const toggled = toggleTodo(todo);
+    const updatedTodos = updateTodo(this.state.todos, toggled);
+    this.setState({todos: updatedTodos});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const newId = generateId();
+    const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false }
+    const updatedTodos = addTodo(this.state.todos, newTodo);
+    this.setState({
+      todos: updatedTodos,
+      currentTodo: '',
+      errorMessage: ''
+    })
+  }
+
+  handleEmptySubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      errorMessage: 'Please supply a todo name'
+    })
+  }
+
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          React Todos
-        </p>
+        <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          <TodoForm handleInputChange={this.handleInputChange}
+                    currentTodo={this.state.currentTodo}
+                    handleSubmit={submitHandler}/>
+                  <TodoList todos={this.state.todos}
+                            handleToggle={this.handleToggle}/>
+
+        </div>
       </div>
     );
   }
